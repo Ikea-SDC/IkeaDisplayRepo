8be9902d5f7854e0d2990c5dda7540f861f8ecb2
brandon <brandonrosenstein@gmail.com>
2018-09-12 14:44:03 -0700

    established carousel, now tweaking

--- Ikea FEC/Client/src/components/TopBar.jsx
+++ Ikea FEC/Client/src/components/TopBar.jsx
@@ -6,12 +6,12 @@ let TopBar = (props) =>(
   <div className={style.bar}>
   <img width="90px" height="33px" src="https://ww8.ikea.com/ext/iplugins/en_US/development/plugins/newHeader/images/logoIKEA.svg"></img>
 
-  <div>Products</div>
-  <div>Inspiration</div>
-  <div>New</div>
-  <div>Offers</div>
-  <div>Services</div>
-  <div>Back to College</div>
+  <div className={style.words}>Products</div>
+  <div className={style.words}>Inspiration</div>
+  <div className={style.words}>New</div>
+  <div className={style.words}>Offers</div>
+  <div className={style.words}>Services</div>
+  <div className={style.words}>Back to College</div>
   <input type="text"></input>
   <div className={style.icons}>
   <img width="30px" height="30px" src="https://ww8.ikea.com/ext/iplugins/en_US/development/plugins/responsiveHeader/images/nav-shopping-list.svg"></img>
