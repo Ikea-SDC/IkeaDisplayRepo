bbbb243c1130d7c1dbc1e79242c18555ecb1c23f
Hannah Holzmann <hholzmann16@gmail.com>
2018-10-09 19:17:02 -0700

    redis works best for server without pm2 instance, also updated artillery test file

--- Ikea_FEC/server/router.js
+++ Ikea_FEC/server/router.js
@@ -5,6 +5,7 @@ var cache = require("express-redis-cache")();
 router
   .route("/item/:id")
   .get(cache.route(), controller.getData)
+  // .get(controller.getData)
   .post(controller.addData)
   .delete(controller.deleteData)
   .put(controller.editData);
